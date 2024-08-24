f44186d72ba82d4cbf4bca71a792a9fb24138b5c
Robino-CK <robin.kollmann@yahoo.de>
2024-08-24 10:42:52 +0200

    draft qlearning

--- solver.py
+++ solver.py
@@ -4,10 +4,47 @@ import planes
 import planes.gui
 import time
 import random
+import numpy as np
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 700
 
 BLACK = (0, 0, 0)
+class QAgent():
+    def __init__(self):
+        
+        self.n_states = (1,3**4,2)  # 4 options + already selected for 12 cards
+        self.n_actions = (12,) # TODO: Wif nothing works, problem!
+
+        self.qtable = np.zeros(self.n_states + self.n_actions)
+        self.alpha = 0.1
+        self.gamma = 0.9
+        self.epsilon = 0.1
+    def get_state(self,cards):
+        state = []
+        for i,card in enumerate(cards):
+            card_state = 0
+            card_state += set.colors.index(card.color)
+            card_state += set.shapes.index(card.shape) * 3
+            card_state += set.numbers.index(card.number) * 9
+            card_state += set.shades.index(card.shade) * 27
+            clicked =  1 if card.been_clicked else 0
+            state.append([i,card_state,clicked])
+        return np.array(state)
+    
+    
+    
+    def get_action(self,cards):
+        state = self.get_state(cards)
+        if np.random.uniform() < self.epsilon:
+            action = np.random.choice(self.n_actions[0])
+        else:
+            action = np.argmax(self.qtable[state])
+        cards[action].been_clicked = True
+        return action
+     
+    def perceive_reward(self, reward, action, cards):
+        state = self.get_state(cards)
+        self.qtable[state + (action,)] = self.qtable[state + (action,)] + self.alpha * (reward + self.gamma * np.max(self.qtable[state]) - self.qtable[state + (action,)])
 
 def random_action(cards):
     selected_cards = random.choices(cards, k=3)
@@ -18,12 +55,23 @@ def random_action(cards):
 def clear_selection(cards):
     for card in cards:
         card.been_clicked = False
+        
+def evualate_selection(cards):
+    selected = []
+    for card in cards:
+        if card.been_clicked:
+            selected.append(card)
+    if len(selected) != 3:
+        return -100
+    if set.check_set(selected[0], selected[1], selected[2]):
+        return 100
+    return -20
 def run_game():
     pygame.init()
     size = (WINDOW_WIDTH, WINDOW_HEIGHT)
     screen = planes.Display(size)
     screen.grab = False
-
+    agent = QAgent()
 
     screen.image.fill(BLACK)
     model = set.Model()
@@ -37,13 +85,21 @@ def run_game():
         for event in events:
             if event.type == pygame.QUIT:
                 raise SystemExit
-        sets_found = model.game.sets_found
-        random_action(model.game.in_play_cards)
+        #sets_found = model.game.sets_found
+        #random_action(model.game.in_play_cards)
+        prev_action = None
+        for i in range(3):
+            action = agent.get_action(model.game.in_play_cards)
+            if prev_action == action:
+                agent.perceive_reward(-100, action, model.game.in_play_cards)
         
+        reward = evualate_selection(model.game.in_play_cards)
+        print(reward)
+        agent.perceive_reward(reward,action,model.game.in_play_cards)
         screen.process(events)
         model.update()
-        if model.game.sets_found > sets_found:
-            print("found set")
+        #if model.game.sets_found > sets_found:
+        #    print("found set")
             #print("hi")
         clear_selection(model.game.in_play_cards)
         
